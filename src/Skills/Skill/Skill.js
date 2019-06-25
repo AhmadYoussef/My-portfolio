86df9fa18777c0f24147617d85449eff99a00a18
AhmadYoussef <Ahmad.k.youssef@gmail.com>
2019-06-25 23:46:28 +0200

    sa

--- src/Skills/Skill/Skill.js
+++ src/Skills/Skill/Skill.js
@@ -3,9 +3,19 @@ import classes from './Skill.module.scss';
 
 
 const Skill = () => {
+
     return (
-        <div>
-            skill
+        <div className={classes.shieldPres}>
+            <div className={classes.shield}>
+                <div className={classes.front}>
+                    <img src={require('../../assets/images/languageLogo/html5.svg')} alt="Avatar" />
+                </div>
+                <div className={classes.back}>
+                    <p>Architect & Engineer</p>
+                    <p>We love that guy</p>
+                </div>
+                <div className={classes.triangle}></div>
+            </div>
         </div>
     );
 }
