8b5255cdffa9f693cddd9fae8474a34b425aeb55
AhmadYoussef <Ahmad.k.youssef@gmail.com>
2019-07-01 14:58:10 +0200

    ss

--- src/server.js
+++ src/server.js
@@ -33,5 +33,7 @@ app.post('/sendMail', function (req, res) {
 
 });
 
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
-app.listen(8080, () => console.log(`http://localhost:8080`));
+app.listen(port, host, () => console.log(`Server started on ${port}`));
